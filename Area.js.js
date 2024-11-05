38a5cf11d19296180501408b7ce5a33e603f0401
richardayong9@gmail.com <richardayong9@gmail.com>
2024-11-05 01:41:06 -1200

    first commit

---
+++ Area.js.js
@@ -0,0 +1,14 @@
+
+//defining the function//
+
+function calculateArea (width, height) {
+    result = width * height;
+    return result;
+
+
+    
+}
+
+//Calling the function
+calculateArea(5, 10)
+
